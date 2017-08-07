8c71dc5bc9a00be112bde52836b3b5825d4a9b1f
leon <jeioncs@gmail.com>
2017-08-07 23:29:36 +0200

    Arreglado el beep, posicionamiento al final y limpieza de histórico.

---
+++ phpSimpleChat/limpiarHistorial.php
@@ -0,0 +1,33 @@
+<?php
+
+function generateRandomString($length = 10) {
+	return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+} 
+
+$filename=generateRandomString().".txt";
+
+if($_POST['enviar']){
+	$user = $_POST['user']; 
+	$password = $_POST['password'];
+   
+	if ($user =="admin" && $password=="admin"){
+		echo "Has borrado el fichero de historial";
+		$chat=file_get_contents("historial.txt");
+		file_put_contents("/tmp/".$filename, $chat);
+		file_put_contents("historial.txt","");
+	}
+
+}
+else{
+
+echo <<<FORM
+<form method="post" >
+Usuario: <input type="text" id="user" name="user" size="20" value="$_POST[user]"><br>
+Password: <input type="password" id="password" name="password" size="20" value="$_POST[password]"><br>
+<input type="submit" VALUE="Enviar"  name="enviar" id="enviar" size=33><BR>
+</form>
+FORM;
+
+}
+
+?> 
