417ae0b3756f5b8dfb1a91571e1f03cd4fc283f1
fatperbot <fatima.perez.boton@gmail.com>
2017-09-26 15:35:05 +0200

    Comentarios JS transición sencilla. Pruebas Google y CSS Fonts.

--- FadeIn-FadeOUT/fade.js
+++ FadeIn-FadeOUT/fade.js
@@ -1,3 +1,11 @@
+// Para añadir el efecto de desvanecimiento justo antes de descargar la página 
 window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
+
+/* Sintaxis: element.addEventListener(event, function, useCapture);
+ * The first parameter is the type of the event (like "click" or "mousedown"). 
+ * The second parameter is the function we want to call when the event occurs.
+ * The third parameter is a boolean value specifying whether to use event bubbling 
+ * or event capturing. This parameter is optional.
+ */ 
